62947fc49f87f68b2d0ed5349a04730040e3858d
Steven Conaway <steven.conaway@icloud.com>
2018-12-02 10:19:02 -0800

    Add BBGCID for iPhone 9,1 and 9,2

--- tsschecker/tsschecker.c
+++ tsschecker/tsschecker.c
@@ -130,8 +130,8 @@ static struct bbdevice bbdevices[] = {
     
     {"iPhone9,1", 2315222105},
     {"iPhone9,2", 2315222105},
-//    {"iPhone9,3", 1421084145},
-//    {"iPhone9,4", 1421084145},
+    {"iPhone9,3", 1421084145},
+    {"iPhone9,4", 1421084145},
     {"iPhone10,1", 2315222105},
     
     {"iPad1,1", 0},
